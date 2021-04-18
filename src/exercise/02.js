b32556b0a26feb382bdb34ac9a0bfc9bdadc69e1
Yohann Legrand <yohann.legrand-ext@believe.com>
2021-04-18 19:19:35 +0200

    exercise 2 - support dom component children

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -8,19 +8,19 @@ function Toggle({children}) {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  return React.Children.map(children, child =>
-    React.cloneElement(child, {
-      on,
-      toggle,
-    }),
-  )
+  return React.Children.map(children, child => {
+    if (allowedTypes.includes(child.type)) {
+      return React.cloneElement(child, {on, toggle})
+    }
+    return child
+  })
 }
 
 const ToggleOn = ({on, children}) => (on ? children : null)
-
 const ToggleOff = ({on, children}) => (on ? null : children)
-
 const ToggleButton = ({on, toggle}) => <Switch on={on} onClick={toggle} />
+
+const allowedTypes = [ToggleOn, ToggleOff, ToggleButton]
 
 function App() {
   return (
@@ -28,6 +28,7 @@ function App() {
       <Toggle>
         <ToggleOn>The button is on</ToggleOn>
         <ToggleOff>The button is off</ToggleOff>
+        <span>Hello</span>
         <ToggleButton />
       </Toggle>
     </div>
